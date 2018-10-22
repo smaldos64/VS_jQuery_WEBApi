223be3b65624f7df8dbb13e5a197c4dc6f0d1a01
Lars Thise Pedersen <ltpe@techcollege.dk>
2018-10-22 12:55:02 +0200

    Add project files.

---
+++ jQuery2/Project_Scripts/IndexHelper.js
@@ -0,0 +1,5 @@
+﻿var StudentOperation_ENUM;
+(function (StudentOperation_ENUM) {
+    StudentOperation_ENUM[StudentOperation_ENUM["Save_Student_Data"] = 0] = "Save_Student_Data";
+    StudentOperation_ENUM[StudentOperation_ENUM["Edit_Student_Data"] = 1] = "Edit_Student_Data";
+})(StudentOperation_ENUM || (StudentOperation_ENUM = {}));
---
+++ jQuery2/Project_Scripts/Settings.js
@@ -0,0 +1,5 @@
+﻿//const WEB_API_URL_Student = "http://localhost:55830/api/StudentName";
+
+//const WEB_API_URL_Student = "http://ltpe4.web.techcollege.dk/api/StudentName";
+
+const WEB_API_URL_Student = "http://webapisimple.buchwaldshave34.dk/api/StudentName";
